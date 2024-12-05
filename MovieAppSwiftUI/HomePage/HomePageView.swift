1217f6d3f5034e43299a5e90efaa867362b3b6eb
diana pava <dianapava65@gmail.com>
2024-12-05 17:00:46 -0500

    create homePage

---
+++ MovieAppSwiftUI/HomePage/HomePageView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovieAppSwiftUI
+//
+//  Created by Diana Pava Avila on 4/12/24.
+//
+
+import SwiftUI
+
+struct HomePageView: View {
+    
+    var body: some View {
+        
+    }
+}
+
+    
+
+#Preview {
+    HomePageView()
+}
---
+++ MovieAppSwiftUI/HomePageView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovieAppSwiftUI
+//
+//  Created by Diana Pava Avila on 4/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        
+    }
+}
+
+    
+
+#Preview {
+    ContentView()
+}
